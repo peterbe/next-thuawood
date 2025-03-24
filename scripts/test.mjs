7566df094c8400bef9a3b05be590975c66bfcd9e
Peter Bengtsson <mail@peterbe.com>
2025-03-24 08:48:53 -0400

    upgrade next (#33)
    * upgrade next
    
    * add test
    
    * regular fetch

---
+++ scripts/test.mjs
@@ -0,0 +1,30 @@
+const BASE_URL = process.env.BASE_URL || "http://localhost:3000";
+const URLS = ["/", "/bilder", "/kontakt", "/bilder/frank-zappa"];
+
+main();
+
+async function main(attempts = 3) {
+  for (const url of URLS.map((pathname) => `${BASE_URL}${pathname}`)) {
+    console.log(url, "...");
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Failed to fetch ${url}`);
+      }
+      const html = await response.text();
+      const titleRegex = /<title>([^<]+)<\/title>/;
+      console.log({ title: titleRegex.exec(html)[1] });
+    } catch (error) {
+      console.error("ERROR:", error);
+      if (attempts > 0) {
+        console.log("Retrying soon...");
+        await sleep(2000);
+        return main(attempts - 1);
+      }
+    }
+  }
+}
+
+async function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
