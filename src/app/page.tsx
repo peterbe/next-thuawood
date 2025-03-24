fd98f1dc560ad8b7608fc0e67f1133b538542574
Peter Bengtsson <mail@peterbe.com>
2025-03-24 08:52:30 -0400

    Upgrade dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,14 +10,14 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <div style={{ padding: 100 }}>
+    <div style={{ padding: 100, textAlign: "center" }}>
       <hgroup>
         <h1>Välkommen till Thuas Trägubbar</h1>
         <p>Thua Bengtsson&apos;s webbplats med bilder på snideri</p>
       </hgroup>
       <Picture src="/images/ubat.jpg" alt="I ubåt med hatt" />
 
-      <p style={{ margin: 100, textAlign: "center", fontSize: "120%" }}>
+      <p style={{ margin: 100, fontSize: "120%" }}>
         <Link href="/bilder">All bilder &rarr;</Link>
       </p>
     </div>
